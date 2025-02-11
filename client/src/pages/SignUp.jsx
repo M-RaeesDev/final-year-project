559cba4f621c1d2136398a20c20b09500f004e07
M-RaeesDiv <raeesawan361@gmail.com>
2025-02-11 14:46:49 +0500

    Complete Sign In Ui and Functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -50,14 +50,14 @@ export default function SignUp() {
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>sign Up</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4 '>
-        <input type="text" placeholder='username' 
-        className='border p-3 rounded-lg' id='username' onChange={handleChange} />
+        <input type="username" placeholder='username' 
+        className='border p-3 rounded-lg' id='username' required onChange={handleChange} />
 
-        <input type="text" placeholder='email' 
-        className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
+        <input type="email" placeholder='email' 
+        className='border p-3 rounded-lg' id='email' required onChange={handleChange}/>
 
-        <input type="text" placeholder='password' 
-        className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
+        <input type="current-password" placeholder='password' 
+        className='border p-3 rounded-lg' id='password' required onChange={handleChange}/>
         <button disabled = {loading} className='bg-slate-700 text-white p-3
         rounded-lg uppercase hover:opacity-95 
         disabled:opacity-80'>
