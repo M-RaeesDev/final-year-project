3da0e0de413a014d836d67382efa28da59867a07
M-RaeesDiv <raeesawan361@gmail.com>
2025-02-16 19:35:01 +0500

    Complete Image Upload Functionalty

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,3 +1,23 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: "final-year-project-29ef6.firebaseapp.com",
+//   projectId: "final-year-project-29ef6",
+//   storageBucket: "final-year-project-29ef6.firebasestorage.app",
+//   messagingSenderId: "1091217014294",
+//   appId: "1:1091217014294:web:2648016c129ea286d03f37"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -6,11 +26,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "final-year-project-29ef6.firebaseapp.com",
-  projectId: "final-year-project-29ef6",
-  storageBucket: "final-year-project-29ef6.firebasestorage.app",
-  messagingSenderId: "1091217014294",
-  appId: "1:1091217014294:web:2648016c129ea286d03f37"
+  authDomain: "goldenoutfit-d1701.firebaseapp.com",
+  projectId: "goldenoutfit-d1701",
+  storageBucket: "goldenoutfit-d1701.appspot.com",
+  messagingSenderId: "329714258527",
+  appId: "1:329714258527:web:7ed1d3afdf7f586a8c01ea"
 };
 
 // Initialize Firebase
