6947146cae17747d9498904292f4e4dc2e22f00f
M-RaeesDiv <raeesawan361@gmail.com>
2025-02-18 12:52:39 +0500

    Complete User Delete Functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
-import OAuth from '../components/OAuth';
+// import OAuth from '../components/OAuth';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -61,7 +61,7 @@ export default function SignIn() {
         disabled:opacity-80'>
           {loading ? "loading..." : "sign In"}
         </button>
-        <OAuth/>
+        {/* <OAuth/> */}
       </form>
 
       <div className='flex gap-2 mt-5'>
